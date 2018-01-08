bde4631e048c8626b5ea5c18cab9e84e227c902d
jaimejahuey <jahuey777@hotmail.com>
2018-01-07 23:59:25 -0500

    Finished tutorial.

--- app/src/main/java/com/jaimejahuey/realmtutorial/MainActivity.java
+++ app/src/main/java/com/jaimejahuey/realmtutorial/MainActivity.java
@@ -19,7 +19,7 @@ import java.util.UUID;
 import io.realm.Realm;
 import io.realm.RealmResults;
 
-//Jaime
+//Jaime Jahuey
 public class MainActivity extends AppCompatActivity {
 
     private Realm mRealm;
